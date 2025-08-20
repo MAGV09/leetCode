fe7197a3f2455384f287d567697a54f2002a97f0
MAGV09 <mohamedadel2002320@gmail.com>
2025-08-20 20:26:59 +0300

    add better solution for containDuplicate

--- containDuplicate/containDuplicate.js
+++ containDuplicate/containDuplicate.js
@@ -1,13 +1,20 @@
-let nums = [1];
-function hasDuplicate() {
-  for (let i = 0; i<nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] == nums[i]) {
-        return true;
-      }
-      
-    }
-  }
-  return false
+// let nums = [1];
+// function hasDuplicate() {
+//   for (let i = 0; i<nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] == nums[i]) {
+//         return true;
+//       }
+
+//     }
+//   }
+//   return false
+// }
+// console.log(hasDuplicate());
+
+let nums=[1,2,3,3]
+
+function hasDuplicate(nums) {
+  return nums.length !== new Set(nums).size;
 }
-console.log(hasDuplicate());
+console.log(hasDuplicate(nums));
