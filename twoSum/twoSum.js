d1bb0d4dca28202d8ad1164f9b4322f296110cf1
MAGV09 <mohamedadel2002320@gmail.com>
2025-08-24 13:54:17 +0300

    add better solution o(n)

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,13 +1,29 @@
-let arr = [3, 4, 5, 6];
-let arr2 = [];
+'use strict';
+// let arr = [3, 4, 5, 6];
+// let arr2 = [];
+// function twoSum(target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] == target) {
+//         arr2.push(i, j);
+//         console.log(arr2);
+//       }
+//     }
+//   }
+// }
+// twoSum(10);
+
+let nums = [3, 4, 6, 5];
 function twoSum(target) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] == target) {
-        arr2.push(i, j);
-        console.log(arr2);
-      }
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let j = map.get(target - nums[i]);
+    if (nums[i] + nums[j] === target && i !== j) {
+      return [i, j];
     }
   }
 }
-twoSum(10);
+console.log(twoSum(7));
