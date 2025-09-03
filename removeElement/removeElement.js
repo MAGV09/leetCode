ac9181b62b40d4cb4caa162af1116ed66dab5f0b
MAGV09 <mohamedadel2002320@gmail.com>
2025-09-03 07:32:47 +0300

    add better solution for removeElement

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -9,7 +9,7 @@
 // const k = nums.length;
 // return k;
 
-const nums = [0,1,2,2,3,0,4,2];
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
 // const removeElement = function (nums, val) {
 //   const filteredArr = nums.filter((item) => item !== val);
 //   for(let i=0;i<filteredArr.length;i++){
@@ -19,28 +19,18 @@ const nums = [0,1,2,2,3,0,4,2];
 // };
 // console.log(removeElement(nums, 3));
 
-//intialize 2 pointers one at the start od the arr and one at the end.
-//if the element at the 1st pointer is equal to val then replace it with element at the 2nd pointer
-//increment 1st pointer and decrement 2nd pointer
-//if the element at the 1st pointer isn't equal to val increment only
-//if the element at the 2nd pointer is equal to val decrement it.
-//when 1st pointer and 2nd pointer equal each other end.
+//intialize a pointer k at 0
+//loop over the array
+//if the the nums[i] != val then nums[k] =nums[i] and increment k
+
 function removeElement(nums, val) {
-  let j = nums.length - 1;
-  for (let i = 0; i < nums.length; ) {
-    if (nums[j] === val) {
-      j--;
-    }
-    if (nums[i] === val && nums[j] !== val) {
-      [nums[i],nums[j]]=[nums[j],nums[i]]
-      
-    } else if (nums[i] !== val && nums[j] != val) {
-      i++;
-    }
-    if (i === j) {
-      break;
+  let k = 0;
+  for (let num of nums) {
+    if (num !== val) {
+      nums[k] = num;
+      k++;
     }
   }
-  return j+1;
+  return k
 }
-console.log(removeElement(nums,2));
+console.log(removeElement(nums, 2));
